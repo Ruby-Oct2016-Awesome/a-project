0f68348ed65d4d83d707d74bd1812f8262daf02b
Andrew <andrewjihoonc@gmail.com>
2016-11-21 23:54:36 +0700

    Added the necessary pages and features for user to logging in, signing up, and logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protect_from_forgery with: :null_sessions
+  helper_method :current_user, :authenticate_user
+
+  def current_user
+  	User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+  	unless current_user
+  		flash.now[:notice] = "You must be logged in to get full access to our tickets!" 
+  		redirect_to sessions_path(:new)
+  	end
+  end
 end
