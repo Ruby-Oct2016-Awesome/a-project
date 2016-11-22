caa4dd8a194e87e8ddf96a97fb66bd1e3940a80f
Dang Quoc Huy <huysamdua@yahoo.com>
2016-11-22 14:38:57 +0700

    support bicycles#index, bicycles#show actions

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,2 +1,33 @@
 class BicyclesController < ApplicationController
+  before_action :set_bicycle, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bicycles = Bicycle.all.order(created_at: :desc)
+  end
+
+  def new
+    @bicycle = Bicycle.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_bicycle
+    @bicycle = Bicycle.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
-	root 'home#index'
+  root 'home#index'
 
-	delete "sign_out" => "sessions#destroy"
+  delete "sign_out" => "sessions#destroy"
 
-	resources :sessions, only: [:new, :create, :destroy]
-	resources :users
-
-	resources :stations
-
-	resources :trips
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :stations
+  resources :trips
+  resources :bicycles
 
 end
