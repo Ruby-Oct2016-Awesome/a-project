0a3754c840d490097279fb099c1d7937d8bcb350
Andrew <andrewjihoonc@gmail.com>
2016-11-23 01:30:46 +0700

    adding main styles to our site

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   delete "sign_out" => "sessions#destroy"
-
-<<<<<<< HEAD
 	get 'my_team' => "teams#my_team"
 
 	resources :sessions, only: [:new, :create, :destroy]
@@ -14,10 +12,9 @@ Rails.application.routes.draw do
 
 	resources :teams
 	resources :users
-=======
+
   resources :trips
   resources :bicycles
   resources :vouchers
->>>>>>> 6c5a37f8382bdbb11804bb7c0b487b5b4299aac7
 
 end
