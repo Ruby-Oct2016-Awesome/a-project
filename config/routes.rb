b5285af0d4947c0df29fa31b5ba67cde3af18ec3
Dang Quoc Huy <huysamdua@yahoo.com>
2016-11-23 14:43:01 +0700

    add 2 samples single page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-
   delete "sign_out" => "sessions#destroy"
+  get "/pages/*page" => "pages#show"	# *page -> [page] as an array
+  # get "/pages/:page" => "pages#show"	# :page as a signle value
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
